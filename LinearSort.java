453239b7a9d1e322efbbb505178d165a9e96d925
Apeksha.m <apeksham532@gmail.com>
2026-07-21 22:07:14 +0530

    Create LinearSort.java
    Linner sort  in an given array

---
+++ LinearSort.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public static void main( ){
+  int [] nums ={4,8,15,16,23};
+  int key=16;
+  int foundIndex =-1;
+  for(int i=0; i<num.length;i++){
+    if(nums[i]== key){
+      foundIndex=i;
+      break;
+    }
+  }
+  if(foundIndex i=-1){
+    System.out.println("found. at index:"+foundIndex);
+  }else{
+  } System.out.println("not found");
+}
